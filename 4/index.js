e36f0bdc6e48bac6f2f6a2e9dfb46a6529544c76
Cruceru Lucas <lucascruceru@gmail.com>
2018-06-19 23:11:50 +0300

    last commit

--- 4/index.js
+++ 4/index.js
@@ -7,13 +7,15 @@ var app = new Vue({
     methods: {
         generateColor : function () {
             this.colors.push('#'+Math.floor(Math.random()*16777215).toString(16));
-        }
+        },
+
     },
     computed: {
-        filteredList() {
-            return this.colors.filter(color => {
-                return color.toLowerCase().includes(this.search.toLowerCase())
+        searchColor: function () {
+            this.colors.filter((color) => {
+                return color.match(this.search);
             })
         }
     }
+
 });
